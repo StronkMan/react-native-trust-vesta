d0e6cf20254b7f83d0cd1c3a635dec8e67b1f3d8
ken <kenneth@fluz.app>
2020-08-21 00:33:05 -0700

    build(android): bump

--- android/src/main/java/com/reactlibrary/TrustVestaModule.java
+++ android/src/main/java/com/reactlibrary/TrustVestaModule.java
@@ -2,62 +2,59 @@ package com.reactlibrary;
 import android.app.Application;
 import android.os.Handler;
 import android.util.Log;
-
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
 import com.vesta.sdk.VestaDataCollector;
 
-
-
 public class TrustVestaModule extends ReactContextBaseJavaModule {
 
-    private final ReactApplicationContext reactContext;
+  private final ReactApplicationContext reactContext;
 
-    public TrustVestaModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-    }
+  public TrustVestaModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
 
-    @Override
-    public String getName() {
-        return "TrustVesta";
-    }
+  @Override
+  public String getName() {
+    return "TrustVesta";
+  }
 
-
-    @ReactMethod
-    public void initializeDataCollectorService(final ReadableMap options, final Callback successCallback,
-                                               final Callback errorCallback) {
-        try{
-            Handler mainHandler = new Handler(this.reactContext.getMainLooper());
-            Runnable startDataCollector = new Runnable() {
-                @Override
-                public void run() {
-                    VestaDataCollector.start( (Application) reactContext.getApplicationContext(),
-                            options.getString("webSessionID"), options.getString("loginID"));
-                }
-            };
-
-            mainHandler.post(startDataCollector);
-
-            successCallback.invoke(true);
-        }catch(Exception e){
-            e.printStackTrace();
-            errorCallback.invoke(e.getMessage());
+  @ReactMethod
+  public void initializeDataCollectorService(final ReadableMap options,
+                                             final Callback successCallback,
+                                             final Callback errorCallback) {
+    try {
+      Handler mainHandler = new Handler(this.reactContext.getMainLooper());
+      Runnable startDataCollector = new Runnable() {
+        @Override
+        public void run() {
+          VestaDataCollector.start(
+              (Application)reactContext.getApplicationContext(),
+              options.getString("webSessionID"), options.getString("loginID"));
+          successCallback.invoke(true);
         }
-
+      };
+      mainHandler.post(startDataCollector);
+    } catch (Exception e) {
+      e.printStackTrace();
+      errorCallback.invoke(e.getMessage());
     }
+  }
 
-    @ReactMethod
-    public void sendLocation(final ReadableMap location, final Callback successCallback,
-                                               final Callback errorCallback) {
-        try {
-            VestaDataCollector.sendLocationData(location.getDouble("lat"), location.getDouble("long"));
-            successCallback.invoke(true);
-        }catch(Exception e){
-            errorCallback.invoke(e.getMessage());
-        }
+  @ReactMethod
+  public void sendLocation(final ReadableMap location,
+                           final Callback successCallback,
+                           final Callback errorCallback) {
+    try {
+      VestaDataCollector.sendLocationData(location.getDouble("lat"),
+                                          location.getDouble("long"));
+      successCallback.invoke(true);
+    } catch (Exception e) {
+      errorCallback.invoke(e.getMessage());
     }
+  }
 }
