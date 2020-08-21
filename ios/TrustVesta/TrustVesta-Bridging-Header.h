6f39af96d4ec59316bf8b8b78bac10ad9d8f1ec9
ken <kenneth@fluz.app>
2020-08-21 03:54:25 -0700

    feat(dc): switch to swift

---
+++ ios/TrustVesta/TrustVesta-Bridging-Header.h
@@ -0,0 +1,6 @@
+#import <React/RCTBridgeModule.h>
+@interface RCT_EXTERN_MODULE(CalendarManager, NSObject)
+
+RCT_EXTERN_METHOD(initializeDataCollectorService:(NSDictionary *)options (RCTResponseSenderBlock *)callback)
+
+@end
---
+++ ios/TrustVesta/TrustVesta.swift
@@ -0,0 +1,24 @@
+//
+//  TrustVesta.swift
+//  TrustVesta
+//
+//  Created by Kenneth on 8/21/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import DataCollector
+import Foundation
+
+@objc(TrustVesta)
+class TrustVesta: NSObject {
+    @objc(initializeDataCollectorService:callback:)
+    func initializeDataCollectorService(_ options: NSDictionary, callback:RCTResponseSenderBlock) {
+        DataCollectorService.default.start(withSessionKey: options["webSessionID"] as! String, loginID: options["loginID"] as! String, sandboxEnabled: true) { success in
+            if success {
+                callback(true);
+            }else{
+                callback(false);
+            }
+        }
+    }
+}
